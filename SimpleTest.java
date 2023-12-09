c50cebf415e9fcaf894598bc480eded8e6059f66
Naran Nathan <nnath003@ucr.edu>
2023-12-08 20:35:33 -0800

    Final changes

---
+++ SimpleTest.java
@@ -0,0 +1,30 @@
+import edu.berkeley.cs.jqf.fuzz.Fuzz;
+import edu.berkeley.cs.jqf.fuzz.JQF;
+import org.junit.runner.RunWith;
+
+@RunWith(JQF.class)
+public class SimpleTest {
+    
+    private void checkEven(Integer a) {
+        if (a % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+
+    private void checkGreaterThanHundred(Integer a) {
+        if (a > 100)
+            System.out.println("Greater than 100");
+        else
+            System.out.println("Less than or equal to 100");
+    }
+
+    @Fuzz
+    public void testSimpleTest(Integer a) {
+        int valueOfA = a & 0xFF;
+        System.out.println("------ Generated input: " + valueOfA + " ------");
+
+        checkEven(valueOfA);
+        checkGreaterThanHundred(valueOfA);
+    }
+}
